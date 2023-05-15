bcb1c7f90336703f772c2ee920ad67085ebf82aa
Prakhar Sharma <1915438@swansea.ac.uk>
2023-05-15 19:05:41 +0100

    string and function manipulation

--- HTML, CSS and Javascript/2. More javascript basics/6. 2 buttons tutorial/index.js
+++ HTML, CSS and Javascript/2. More javascript basics/6. 2 buttons tutorial/index.js
@@ -1,4 +1,17 @@
 function increment() {
     document.getElementById("count").innerText ++  // increment the counter with every click
-    console.log("The button was clicked")
+    console.log("Increment")
 }
+
+function reset() {
+    document.getElementById("count").innerText = 0
+    document.getElementById("string").innerText = 0
+    console.log("Reset")
+}
+
+
+function save() {
+    document.getElementById("string").innerText += "-" + document.getElementById("count").innerText
+    console.log("Save")
+}
+
