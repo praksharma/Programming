26dfd019e7435a6b611885136b76cf1d88352c7b
Prakhar Sharma <1915438@swansea.ac.uk>
2023-05-15 18:36:21 +0100

    javascript functions and buttons

---
+++ HTML, CSS and Javascript/2. More javascript basics/2. create variables and print stuff/index.js
@@ -0,0 +1,13 @@
+//document.getElementById("count").innerText = 5
+
+let count = 0 // variable
+count++ // ok c++ stuff works
+console.log(count)
+
+let a = 2, b = 3
+
+let c = a * 2
+
+// different ways to print string
+console.log("The value of a is : ", a)
+console.log("The value of b is : "+ b)
