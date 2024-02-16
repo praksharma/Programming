e5375bc87e35f173e91c8653695385b6d0c799f6
Prakhar Sharma <1915438@swansea.ac.uk>
2024-02-16 18:39:34 +0000

    electron basic

---
+++ HTML, CSS and Javascript/6. electron basic/my-electron-app/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+
+// Disable GPU Hardware Acceleration
+app.disableHardwareAcceleration();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
